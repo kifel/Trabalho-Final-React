40fcf8c87603e8419f788071a07677bb1444adf4
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-11-04 12:58:02 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <Route path="/produtos" element={<Produtos />} />
           <Route path="/produtos/:id" element={<Produto />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
