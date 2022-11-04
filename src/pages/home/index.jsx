dd968d73667b3ee0ae73ac05d16cbbfa36d3101a
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-11-04 11:44:43 -0300

    home page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import img from "../../assets/bg.jpg";
+import { ColorName } from "../../components/navbar/styles";
+import { Produtos } from "../produtos";
+import { Imagem } from "./styles";
+
+export const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card text-bg-dark border-0">
+        <Imagem src={img} alt="Background" />
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">TOP <ColorName>SECRET</ColorName></h5>
+            <p className="card-text lead fs-2">A MAIOR LOJA DO MERCADO TEC</p>
+          </div>
+        </div>
+      </div>
+      <Produtos />
+    </div>
+  );
+};
---
+++ src/pages/home/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Imagem = styled.img`
+  max-height: 45rem;
+  min-height: 15rem;
+`;
