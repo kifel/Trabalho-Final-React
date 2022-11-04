63fd6fa0ec4bc1bf72bfef9ad4cb328b94c7025d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-11-04 13:14:50 -0300

    router test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { NavBar } from "./components/navbar";
 import { Cart } from "./pages/cart";
