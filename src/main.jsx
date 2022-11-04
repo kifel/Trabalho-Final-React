818ee7822a2cd61539e7b839f60b094db34b5e9c
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-11-04 11:37:38 -0300

    organização inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "font-awesome/css/font-awesome.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
